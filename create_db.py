9119e819096d1b2cb6f95a939a948be60229c5ed
Rhea Chandragiri <rchand4@terpmail.umd.edu>
2025-10-31 19:26:44 -0400

    File create_db.py from lecture code, updated to support email field.

---
+++ create_db.py
@@ -0,0 +1,39 @@
+from sqlalchemy.dialects.mysql import VARCHAR
+
+from app import app, db
+from models import Student, Major
+import datetime as dt
+
+with app.app_context():
+    db.drop_all()
+    db.create_all()
+
+    # Initial loading of majors
+    majors = ['Aerospace Engineering', 'Biology', 'Civil Engineering', 'Computer Science',
+              'Electrical Engineering', 'Finance', 'Information Systems', 'Marketing',
+              'Mechanical Engineering']
+    for each_major in majors:
+        print(each_major)
+        a_major = Major(major=each_major)
+        db.session.add(a_major)
+        db.session.commit()
+
+    # Initial loading of students first_name, last_name, major_id, birth_date, is_honors
+    students = [
+        {'student_id': '1', 'first_name': 'Robert', 'last_name':'Smith', 'major_id':3,
+            'birth_date': dt.datetime(2005, 6, 1), 'is_honors':1,
+            'email': 'robert.smith@umd.edu'},
+        {'student_id': '2', 'first_name': 'Leo', 'last_name': 'Van Munching', 'major_id':6,
+            'birth_date': dt.datetime(2004, 3, 24), 'is_honors': 0,
+            'email': 'leo.vanmunching@umd.edu'},
+    ]
+
+    for each_student in students:
+        print(f'{each_student["first_name"]} {each_student["last_name"]} inserted into Student')
+        a_student = Student(first_name=each_student["first_name"], last_name=each_student["last_name"],
+                            major_id=each_student["major_id"], birth_date=each_student["birth_date"],
+                            is_honors=each_student["is_honors"], email=each_student["email"])
+        db.session.add(a_student)
+        db.session.commit()
+
+
